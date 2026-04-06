3e49a6df97c42810dc4538ed13baae47b3626312
blankewa <ewan.mcmahon2@gmail.com>
2026-04-06 15:35:13 +0100

    add admin dashboard

--- scripts/login.js
+++ scripts/login.js
@@ -1,13 +1,31 @@
 // login script for admin login page
 
+console.log("Login script loaded");
+
 const loginForm = document.getElementById('login-form');
 const usernameInput = document.getElementById('username');
 const passwordInput = document.getElementById('password');
 
 //check against backend (eventually)
-loginForm.addEventListener('submit', function(event) {
+loginForm.addEventListener('login', function(event) {
     event.preventDefault(); 
     const username = usernameInput.value;
     const password = passwordInput.value;
 
+    console.log('Username:', username);
+    console.log('Password:', password);
+
+    location.replace("/pages/adminDashboard.html");
+
 });
+
+//create login function
+function login() {
+    const username = usernameInput.value;
+    const password = passwordInput.value;
+
+    console.log('Username:', username);
+    console.log('Password:', password);
+
+    location.replace("/pages/adminDashboard.html");
+}
