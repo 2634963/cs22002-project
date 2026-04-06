853120dba12099b40350206fc1dfb668ab6d5120
2624974 <2624974@dundee.ac.uk>
2026-04-06 15:38:38 +0100

    added comments to makePayment.py

--- api/makePayment.py
+++ api/makePayment.py
@@ -1,9 +1,9 @@
-import flask
 import requests
 
 PAYMENT_PROCESSOR_URL = "http://127.0.0.1:3001"
 
 def call(args):
+    # ensure that all of the arguments required are present, else return 400 bad request
     if "cardNumber" not in args:
         return 400
 
@@ -19,6 +19,8 @@ def call(args):
     if "paymentAmountPence" not in args:
         return 400
 
+    # every argument exists, make the call to the payment processor
     response = requests.get(PAYMENT_PROCESSOR_URL, {"cardNumber":args["cardNumber"], "expireMonth":args["expireMonth"], "expireYear":args["expireYear"], "ccv":args["ccv"], "paymentAmountPence":args["paymentAmountPence"]})
 
+    # and return what it said
     return 200 if (200 <= response.status_code < 300) else 400
