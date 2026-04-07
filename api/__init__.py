050f80054adc1465dbabbcfe92703519ba8451a9
2624974 <2624974@dundee.ac.uk>
2026-04-07 17:24:00 +0100

    Changed line ending in api/__init__.py

--- api/__init__.py
+++ api/__init__.py
@@ -5,6 +5,7 @@ import api.log as log
 import api.createPost as createPost
 import api.givePostApproval as givePostApproval
 
+
 # a list of every endpoint that should be accessible to an end user
 # an api function that isnt in this dict can still be called from other python files but will not be accessoible from /api/<endpoint>
 endpoints = {
