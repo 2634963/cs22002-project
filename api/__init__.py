1f87971dba55db6b6c3351d44d48a7dd38c91086
2624974 <2624974@dundee.ac.uk>
2026-04-07 17:12:22 +0100

    New API endpoint

--- api/__init__.py
+++ api/__init__.py
@@ -3,6 +3,7 @@ import api.makePayment as makePayment
 import api.attemptLogin as attemptLogin
 import api.log as log
 import api.createPost as createPost
+import api.givePostApproval as givePostApproval
 
 # a list of every endpoint that should be accessible to an end user
 # an api function that isnt in this dict can still be called from other python files but will not be accessoible from /api/<endpoint>
@@ -25,5 +26,10 @@ endpoints = {
     "createPost":{
         "requires":["title", "content", "authorUserId"],
         "module":createPost
+    },
+
+    "givePostApproval":{
+        "requires":["postId", "approvedOrNot"],
+        "module":givePostApproval
     }
 }
