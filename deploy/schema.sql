96aaf5799417aa128e7cd421fc87a915b6a7a739
2634963 <2634963@dundee.ac.uk>
2026-04-08 13:05:24 +0100

    Create database

---
+++ deploy/schema.sql
@@ -0,0 +1,135 @@
+-- MySQL dump 10.13  Distrib 5.7.33, for Linux (x86_64)
+--
+-- Host: localhost    Database: fessup
+-- ------------------------------------------------------
+-- Server version	5.7.33
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `Comment`
+--
+
+DROP TABLE IF EXISTS `Comment`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `Comment` (
+  `ID` int(11) NOT NULL,
+  `PostID` int(11) NOT NULL,
+  `Content` varchar(2000) NOT NULL,
+  `Approved` tinyint(1) NOT NULL,
+  PRIMARY KEY (`ID`),
+  KEY `PostID` (`PostID`),
+  CONSTRAINT `Comment_ibfk_1` FOREIGN KEY (`PostID`) REFERENCES `Post` (`ID`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `Comment`
+--
+
+LOCK TABLES `Comment` WRITE;
+/*!40000 ALTER TABLE `Comment` DISABLE KEYS */;
+/*!40000 ALTER TABLE `Comment` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `ExtraInfoAccess`
+--
+
+DROP TABLE IF EXISTS `ExtraInfoAccess`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `ExtraInfoAccess` (
+  `PaymentID` int(11) NOT NULL,
+  `UserID` int(11) NOT NULL,
+  `PostID` int(11) NOT NULL,
+  PRIMARY KEY (`PaymentID`),
+  KEY `UserID` (`UserID`),
+  KEY `PostID` (`PostID`),
+  CONSTRAINT `ExtraInfoAccess_ibfk_1` FOREIGN KEY (`UserID`) REFERENCES `User` (`ID`),
+  CONSTRAINT `ExtraInfoAccess_ibfk_2` FOREIGN KEY (`PostID`) REFERENCES `Post` (`ID`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `ExtraInfoAccess`
+--
+
+LOCK TABLES `ExtraInfoAccess` WRITE;
+/*!40000 ALTER TABLE `ExtraInfoAccess` DISABLE KEYS */;
+/*!40000 ALTER TABLE `ExtraInfoAccess` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `Post`
+--
+
+DROP TABLE IF EXISTS `Post`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `Post` (
+  `ID` int(11) NOT NULL,
+  `PosterID` int(11) NOT NULL,
+  `Title` varchar(255) NOT NULL,
+  `Content` varchar(2000) NOT NULL,
+  `ExtraInfo` varchar(2000) DEFAULT NULL,
+  `Approved` tinyint(1) NOT NULL,
+  PRIMARY KEY (`ID`),
+  KEY `PosterID` (`PosterID`),
+  CONSTRAINT `Post_ibfk_1` FOREIGN KEY (`PosterID`) REFERENCES `User` (`ID`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `Post`
+--
+
+LOCK TABLES `Post` WRITE;
+/*!40000 ALTER TABLE `Post` DISABLE KEYS */;
+/*!40000 ALTER TABLE `Post` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `User`
+--
+
+DROP TABLE IF EXISTS `User`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `User` (
+  `ID` int(11) NOT NULL,
+  `PasswordHash` varchar(128) NOT NULL,
+  `Admin` tinyint(1) NOT NULL,
+  PRIMARY KEY (`ID`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `User`
+--
+
+LOCK TABLES `User` WRITE;
+/*!40000 ALTER TABLE `User` DISABLE KEYS */;
+/*!40000 ALTER TABLE `User` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2026-04-08  0:44:50
