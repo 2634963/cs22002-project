d4248037ed064c2b6468e0b2e5bd3392c0538a99
2634963 <2634963@dundee.ac.uk>
2026-04-08 20:23:28 +0100

    Implement login on the frontend

--- scripts/login.js
+++ scripts/login.js
@@ -2,15 +2,11 @@
 
 console.log("Login script loaded");
 
-//connect to james api
-fetch('http://127.0.0.1:5500/api/log?message=hello');
-
 const loginForm = document.getElementById('login-form');
 const usernameInput = document.getElementById('username');
 const passwordInput = document.getElementById('password');
 
-//check against backend (eventually)
-loginForm.addEventListener('submit', function(event) {
+loginForm.addEventListener('submit', async function(event) {
     event.preventDefault();
     const username = usernameInput.value;
     const password = passwordInput.value;
@@ -18,16 +14,29 @@ loginForm.addEventListener('submit', function(event) {
     console.log('Username:', username);
     console.log('Password:', password);
 
-    window.location.href = "/pages/admin/adminDashboard.html";
+    // Send login details to backend and store the response
+    let loginResponseText = "";
+    
+    const loginResponse = await fetch("/api/login", {
+        headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json'
+        },
+        
+        method: "POST",
+        body: JSON.stringify({username: usernameInput,
+                              password: passwordInput})
+    }).then(response => response.text()).then(text => loginResponseText = text);
+
+    if(!loginResponse.ok) {
+        // Login details were incorrect, error
+        document.getElementById('error').textContent = "Invalid username or password.";
+    }
+    else {
+        // Store session token in a cookie
+        document.cookie = "sessionToken=" + loginResponseText;
+
+        // Load admin dashboard
+        window.location.href = "/pages/admin/adminDashboard.html";
+    }
 });
-
-//create login function
-function login() {
-    const username = usernameInput.value;
-    const password = passwordInput.value;
-
-    console.log('Username:', username);
-    console.log('Password:', password);
-
-    window.location.href = "/pages/admin/adminDashboard.html";
-}
