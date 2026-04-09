4295be8a725333b7b4fcb84c0aaf7ff56cab74fe
2624974 <2624974@dundee.ac.uk>
2026-04-09 21:03:59 +0100

    Connected setPostApproval to DB

--- api/adminSetPostApproval.py
+++ api/adminSetPostApproval.py
@@ -1,8 +1,6 @@
-from api._databaseConnection import database
+from api._databaseConnection import database, connection
 
 import flask
-
-print(__file__ + " has nothing to do with the database and must be updated")
 
 def call(args):
     if "postId" not in args["args"]:
@@ -10,5 +8,16 @@ def call(args):
 
     if "approved" not in args["args"]:
         return flask.Response("no approval", 400)
+
+    if args["args"]["approved"] == 1:
+        database.execute(f"update posts set approved=1 where postID='{args["args"]["postId"]}'")
+        connection.commit()
+
+    elif args["args"]["approved"] == 0:
+        database.execute(f"delete from posts where postID='{args["args"]["postId"]}'")
+        connection.commit()
+
+    else:
+        return flask.Response("invalid approval value", 400)
 
     return flask.Response(status=501)
