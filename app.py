f10a66a74caef4c32d0c12bd2712bfa0c02ffc3b
2624974 <2624974@dundee.ac.uk>
2026-04-10 15:38:09 +0100

    Return server error if an unhandled exception is raised

--- app.py
+++ app.py
@@ -74,71 +74,75 @@ def getFile(filePath: str) -> (str | None):
 @app.route("/", defaults={"path":""}) # type: ignore
 @app.route("/<path:path>", methods=["GET", "POST"]) # type: ignore
 def servePage(path):
-    requestArgs = {
-        "args":flask.request.args,
-        "cookies":flask.request.cookies
-    }
+    try:
+        requestArgs = {
+            "args":flask.request.args,
+            "cookies":flask.request.cookies
+        }
 
-    if flask.request.method != "GET":
-        requestArgs["args"] = json.loads(flask.request.data)
+        if flask.request.method != "GET":
+            requestArgs["args"] = json.loads(flask.request.data)
 
-    # blank path means main page
-    if not path:
-        return getFile("./pages/main.html")
+        # blank path means main page
+        if not path:
+            return getFile("./pages/main.html")
 
-    # If a non admin attempts to access the admin dashboard, deny access
-    elif (len(path.split("/")) >= 2) and path.split("/")[1] == "admin" and not userIsAdmin():
-        return flask.Response(getFile("./pages/__adminDenial.html"), 404)
+        # If a non admin attempts to access the admin dashboard, deny access
+        elif (len(path.split("/")) >= 2) and path.split("/")[1] == "admin" and not userIsAdmin():
+            return flask.Response(getFile("./pages/__adminDenial.html"), 404)
 
-    # If a non logged-in user attempts to access anything, redirect them to login
-    elif (path == "pages/post.html" or path == "pages/main.html") and not isLoggedIn():
-        return flask.Response(getFile("./pages/login.html"), 401)
+        # If a non logged-in user attempts to access anything, redirect them to login
+        elif (path == "pages/post.html" or path == "pages/main.html") and not isLoggedIn():
+            return flask.Response(getFile("./pages/login.html"), 401)
 
-    # dont currently have a favicon
-    elif (path == "./favicon.ico") or (path == "favicon.ico"):
-        return flask.Response("no favicon yet", status=404)
+        # dont currently have a favicon
+        elif (path == "./favicon.ico") or (path == "favicon.ico"):
+            return flask.Response("no favicon yet", status=404)
 
-    # disallow access to root directory
-    elif len(path.split("/")) == 1:
-        return flask.Response("invalid path", 401)
+        # disallow access to root directory
+        elif len(path.split("/")) == 1:
+            return flask.Response("invalid path", 401)
 
-    elif path.split("/")[-1].startswith("__"):
-        return flask.Response("no such page", 404)
+        elif path.split("/")[-1].startswith("__"):
+            return flask.Response("no such page", 404)
 
-    # api calls are handled separately to pages
-    elif path.split("/")[0] == "api":
-        splitPath = path.split("/")
+        # api calls are handled separately to pages
+        elif path.split("/")[0] == "api":
+            splitPath = path.split("/")
 
-        # /api with no further details
-        if len(splitPath) == 1:
-            return flask.Response("please use an endpoint", status=404)
+            # /api with no further details
+            if len(splitPath) == 1:
+                return flask.Response("please use an endpoint", status=404)
 
-        # if a page starts with "admin" then only an administrator account can access it
-        elif splitPath[1].startswith("admin"):
-            if not userIsAdmin():
-                return flask.Response("you must be an admin to do that", 403)
+            # if a page starts with "admin" then only an administrator account can access it
+            elif splitPath[1].startswith("admin"):
+                if not userIsAdmin():
+                    return flask.Response("you must be an admin to do that", 403)
 
-        # if the requested endpoint exists
-        if splitPath[1] in api.endpoints:
-            # call said endpoint and get its return code
-            apiResponse = api.endpoints[splitPath[1]]["module"].call(requestArgs) # yes this is hacky but it works
+            # if the requested endpoint exists
+            if splitPath[1] in api.endpoints:
+                # call said endpoint and get its return code
+                apiResponse = api.endpoints[splitPath[1]]["module"].call(requestArgs) # yes this is hacky but it works
 
-            return apiResponse
+                return apiResponse
 
-        return flask.Response(status=404)
+            return flask.Response(status=404)
 
-    # default media type is html
-    mimeType = "text/html"
+        # default media type is html
+        mimeType = "text/html"
 
-    # change it if necessary
-    if path.split(".")[-1] == "css":
-        mimeType = "text/css"
-    elif path.split(".")[-1] == "js":
-        mimeType = "text/javascript"
+        # change it if necessary
+        if path.split(".")[-1] == "css":
+            mimeType = "text/css"
+        elif path.split(".")[-1] == "js":
+            mimeType = "text/javascript"
 
-    page = getFile(path)
+        page = getFile(path)
 
-    if page == None:
-        return flask.Response("no such page", 404)
+        if page == None:
+            return flask.Response("no such page", 404)
 
-    return flask.Response(page, mimetype=mimeType)
+        return flask.Response(page, mimetype=mimeType)
+
+    except:
+        return flask.Response("internal server error", 500)
